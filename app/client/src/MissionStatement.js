1645f0d727fba278751ad704955b809f7f20b4b3
ivanjcollins <ivanjcollins@outlook.com>
2025-11-20 14:20:57 -0500

    Frontend updates

--- app/client/src/MissionStatement.js
+++ app/client/src/MissionStatement.js
@@ -37,9 +37,13 @@ function MissionStatement() {
     return(
         <div className="missionStatementPage">
             <h1 className="MissionTitle">{text[currentLanguage].Mission.MissionTitle}</h1>
+<<<<<<< Updated upstream
             <p>
             This webpage was made to make learning Spanish & English a fun experience for kids! Through these interactive games, kids and people of all ages can build their understanding of a language while having a good time. Our mission is to create a playful learning environment, while still encouraging curiosity and confidence in a new language. By intertwining education with entertainment, this project entices kids to explore bilingualism in a way that makes you feel like you’re playing, not work!
             </p>
+=======
+            <p className="MissionTitle">{text[currentLanguage].Mission.MissionTitle} </p>
+>>>>>>> Stashed changes
 
             <h2 className="MissionTitle">{text[currentLanguage].Mission.AD} </h2>
             <ul>
