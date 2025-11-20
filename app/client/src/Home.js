6a4ef9b6641d9fc08024738ff27780cf66a64953
ivanjcollins <ivanjcollins@outlook.com>
2025-11-20 14:23:01 -0500

    Frontend updates

--- app/client/src/Home.js
+++ app/client/src/Home.js
@@ -42,7 +42,11 @@ function Home() {
       <h1 className="homeTitle">{text[currentLanguage].home.homeTitle}</h1>
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   
+=======
+
+>>>>>>> Stashed changes
 =======
 
 >>>>>>> Stashed changes
@@ -58,7 +62,11 @@ function Home() {
           <h2>{text[currentLanguage].home.game1}</h2>
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
           <p1>{text[currentLanguage].home.game1_des}</p1>
+=======
+          <h2>{text[currentLanguage].home.game1_des}</h2>
+>>>>>>> Stashed changes
 =======
           <h2>{text[currentLanguage].home.game1_des}</h2>
 >>>>>>> Stashed changes
@@ -74,7 +82,11 @@ function Home() {
           <h2>{text[currentLanguage].home.game2}</h2>
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
           <p1>{text[currentLanguage].home.game2_des}</p1>
+=======
+          <h2>{text[currentLanguage].home.game2_des}</h2>
+>>>>>>> Stashed changes
 =======
           <h2>{text[currentLanguage].home.game2_des}</h2>
 >>>>>>> Stashed changes
--- app/client/src/MissionStatement.js
+++ app/client/src/MissionStatement.js
@@ -39,9 +39,13 @@ function MissionStatement() {
             <h1 className="MissionTitle">{text[currentLanguage].Mission.MissionTitle}</h1>
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             <p>
             This webpage was made to make learning Spanish & English a fun experience for kids! Through these interactive games, kids and people of all ages can build their understanding of a language while having a good time. Our mission is to create a playful learning environment, while still encouraging curiosity and confidence in a new language. By intertwining education with entertainment, this project entices kids to explore bilingualism in a way that makes you feel like you’re playing, not work!
             </p>
+=======
+            <p className="MissionTitle">{text[currentLanguage].Mission.MissionTitle} </p>
+>>>>>>> Stashed changes
 =======
             <p className="MissionTitle">{text[currentLanguage].Mission.MissionTitle} </p>
 >>>>>>> Stashed changes
